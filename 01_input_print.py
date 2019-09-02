7a0565f1b01dd798bf1c29bc7a49db4f9cd32dc2
刘éliulujie <LuJieWanAn@163.com>
2019-09-02 20:17:02 +0800

    Initial commit

---
+++ 01_input_print.py
@@ -0,0 +1,17 @@
+'''
+利用input和print写小练习
+time:2019/9/2
+the author:kk
+'''
+
+print('please enter your name:')
+name = input()
+print('please enter  your age:')
+age = input()
+print('please enter your gender:')
+gender = input()
+print('please enter your height')
+height = input()
+print('Please confirm your information again:')
+# print('name:',name,'age:',age,'gender:',gender,'height:'.height)
+print('name:' + name, '\n', 'age:' + age, '\n', 'gender:' + gender, '\n', 'height:' + height)
